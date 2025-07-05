fc6861f9387b1dd5ded0a2188aef9706cdd8c75a
稀神灵梦 <touhouer@vip.qq.com>
2025-07-06 04:39:00 +0800

    fix repeat

--- src/main/java/cc/thonly/polydex2eiv/mixin/ItemFiltersMixin.java
+++ src/main/java/cc/thonly/polydex2eiv/mixin/ItemFiltersMixin.java
@@ -1,9 +1,13 @@
 package cc.thonly.polydex2eiv.mixin;
 
+import cc.thonly.polydex2eiv.util.ClientPolymerItemUtils;
 import de.crafty.eiv.common.overlay.ItemFilters;
 import eu.pb4.polymer.core.api.client.ClientPolymerItem;
 import eu.pb4.polymer.core.impl.client.InternalClientRegistry;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.registry.Registries;
+import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -11,6 +15,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Iterator;
 import java.util.List;
+import java.util.Optional;
 
 @Mixin(ItemFilters.class)
 public class ItemFiltersMixin {
@@ -20,7 +25,18 @@ public class ItemFiltersMixin {
         Iterator<ClientPolymerItem> iterator = ClientPolymerItem.REGISTRY.stream().iterator();
         while (iterator.hasNext()) {
             ClientPolymerItem next = iterator.next();
-            list.add(next.visualStack());
+            ItemStack itemStack = next.visualStack();
+            if (!ClientPolymerItemUtils.isPolymerItem(itemStack)) {
+                continue;
+            }
+            Optional<String> polymerStackId = ClientPolymerItemUtils.getPolymerStackId(itemStack);
+            if (polymerStackId.isEmpty()) {
+                continue;
+            }
+            if (Registries.ITEM.get(Identifier.of(polymerStackId.get())) != Items.AIR) {
+                continue;
+            }
+            list.add(itemStack);
         }
         cir.setReturnValue(list);
     }
